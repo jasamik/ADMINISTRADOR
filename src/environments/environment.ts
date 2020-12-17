ba9405ea58fdbd4bd0572478a424da4e1e47493a
carlos Mejia <jasamik@hotmail.com>
2020-12-16 21:13:37 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDzg8mIQkahRPluuMwAbRFx1cisZLs6xII",
+    authDomain: "sisvent-e836a.firebaseapp.com",
+    databaseURL: "https://sisvent-e836a.firebaseio.com",
+    projectId: "sisvent-e836a",
+    storageBucket: "sisvent-e836a.appspot.com",
+    messagingSenderId: "454125244843",
+    appId: "1:454125244843:web:ae28e39a10138ce32982ee",
+  },
+};
